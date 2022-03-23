a45f1d23775342446af9ad3a61b284effbdb945d
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-03-24 08:50:42 +0900

    초기세팅

---
+++ schemas/data.js
@@ -0,0 +1,29 @@
+//mongoose에서 프로젝트에 필요한 데이터의 형태와 규격 정의
+
+const mongoose = require("mongoose");
+const dataSchema = mongoose.Schema({
+  menuName: {
+    type: String,
+    required: true,
+  },
+  menuName: {
+    type: String,
+    required: true,
+  },
+  menuName: {
+    type: String,
+    required: true,
+  },
+  menuName: {
+    type: String,
+    required: true,
+  },
+  menuName: {
+    type: String,
+    required: true,
+  },
+  menuName: {
+    type: String,
+    required: true,
+  },
+});
---
+++ schemas/index.js
@@ -0,0 +1,15 @@
+//mongoose와 db를 연결
+
+const mongoose = require("mongoose");
+
+//mongoose를 서버 주로의 컬렉션에 연결하고, undefined일때는 무시하고 error는 캐치해서 콘솔에 띄워주는 설정
+const connnect = () => {
+  mongoose
+    .connect("mongodb://localhost:27017/nutrient_db", { ignoreUndefined: true })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+//connnect를 모듈로 내보내겠다는 설정
+module.exports = connnect;
