62d3fca1bb2df8b4062a604b6aa21aef483f9220
jason taekyun Kim <saurus234@gmail.com>
2022-05-23 14:40:22 +0900

    update searchMenu logic

--- controllers/main.js
+++ controllers/main.js
@@ -9,16 +9,18 @@ const search = async (req, res) => {
     return res.status(400).json("검색어를 입력해주세요.");
   }
 
-  //검색어 입력시 타이틀/타입에서 해당 검색어로 검색
-  const searchChallenge = await Menu.find({ menuName: keyword });
+  //검색어 입력시 타이틀에서 해당 검색어로 검색
+  const searchChallenge = await Menu.find({
+    menuName: { $regex: keyword },
+  });
 
-  //검색어와 일치하는 챌린지 없는 경우 예외처리
+  //검색어와 일치하는 메뉴 없는 경우 예외처리
   if (searchChallenge.length === 0) {
     return res
       .status(401)
-      .json({ message: "검색과 일치하는 챌린지가 없습니다." });
+      .json({ message: "검색과 일치하는 메뉴가 없습니다." });
   }
-  console.log("searchChallenge: ", searchChallenge);
+  //   console.log("searchChallenge: ", searchChallenge);
   return res.status(201).json(searchChallenge);
 };
 
