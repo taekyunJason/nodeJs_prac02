9d7ba8675986ffb903629c98fb48d04938aece27
jason taekyun Kim <saurus234@gmail.com>
2022-06-02 22:38:56 +0900

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -4,8 +4,8 @@
 const mongoose = require("mongoose");
 
 //mongoose를 서버 주로의 컬렉션에 연결하고, undefined일때는 무시하고 error는 캐치해서 콘솔에 띄워주는 설정
-const connnect = () => {
-  mongoose
+const connnect = async () => {
+  await mongoose
     .connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
