076334c3893aa404e9c99ee0428402a16d33291e
jason taekyun Kim <saurus234@gmail.com>
2022-06-03 10:24:12 +0900

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 //mongoose와 db를 연결
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const mongoose = require("mongoose");
 
 //mongoose를 서버 주로의 컬렉션에 연결하고, undefined일때는 무시하고 error는 캐치해서 콘솔에 띄워주는 설정
 const connnect = async () => {
